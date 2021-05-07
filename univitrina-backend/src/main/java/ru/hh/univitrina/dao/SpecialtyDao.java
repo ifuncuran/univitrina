b50e808fb9142842d7aad7f93fa807a87f1a3b80
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2021-05-07 12:32:46 +0300

    back-9 add specialty dao

--- univitrina-backend/src/main/java/ru/hh/univitrina/dao/SpecialtyDao.java
+++ univitrina-backend/src/main/java/ru/hh/univitrina/dao/SpecialtyDao.java
@@ -12,6 +12,54 @@ public class SpecialtyDao extends GenericDao {
     super(sessionFactory);
   }
 
+  public Specialty getBySpecialityCode(String specialityCode) {
+    return getSession()
+      .createQuery("SELECT s FROM Specialty s " +
+                      "WHERE s.code = :specialityCode",
+              Specialty.class)
+      .setParameter("specialityCode", specialityCode)
+      .getSingleResult();
+  }
+
+  public List<Specialty> getFilteredByTrainingDirection(Integer page, Integer perPage, Integer trainingDirectionId) {
+    return getSession()
+      .createQuery("SELECT s FROM Specialty s " +
+                      "JOIN FETCH s.trainingDirection td " +
+                      "WHERE td.id = :trainingDirectionId " +
+                      "ORDER BY s.name",
+              Specialty.class)
+      .setParameter("trainingDirectionId", trainingDirectionId)
+      .setFirstResult(page * perPage)
+      .setMaxResults(perPage)
+      .list();
+  }
+
+  public List<Specialty> getFilteredByProfession(Integer page, Integer perPage, Integer professionId) {
+    return getSession()
+      .createQuery("SELECT s FROM Specialty s " +
+                      "JOIN FETCH s.professionSet ps " +
+                      "WHERE ps.id = :professionId " +
+                      "ORDER BY s.name",
+              Specialty.class)
+      .setParameter("professionId", professionId)
+      .setFirstResult(page * perPage)
+      .setMaxResults(perPage)
+      .list();
+  }
+
+  public List<Specialty> getFilteredByUniversity(Integer page, Integer perPage, Integer universityId) {
+    return getSession()
+      .createQuery("SELECT s FROM Specialty s " +
+                      "JOIN FETCH s.universitySet us " +
+                      "WHERE us.id = :universityId " +
+                      "ORDER BY s.name",
+              Specialty.class)
+      .setParameter("universityId", universityId)
+      .setFirstResult(page * perPage)
+      .setMaxResults(perPage)
+      .list();
+  }
+
   public List<Specialty> getSearchSuggestion(String prefix, Integer limit) {
     return getSession()
         .createQuery("SELECT sp FROM Specialty sp " +
