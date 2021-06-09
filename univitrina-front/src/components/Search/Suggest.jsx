68c6bcfb3a39cfe1d89a44c627a2632f27bc1c5a
ifuncuran <ifuncuran@gmail.com>
2021-06-09 22:20:52 +0300

    front-27 add routing to card by click on suggest

--- univitrina-front/src/components/Search/Suggest.jsx
+++ univitrina-front/src/components/Search/Suggest.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import { useHistory } from 'react-router-dom';
 import { Box } from '@material-ui/core';
 import useStyles from './style';
 
 function Suggest(props) {
   const classes = useStyles();
-  const { inputValue, setInputValue, suggests } = props;
+  const { inputValue, setInputValue, suggests, pagePath } = props;
+  const history = useHistory();
 
   return (
     <Autocomplete
       freeSolo
-      value={inputValue}
-      onChange={(event, newValue) => {
-        setInputValue(newValue);
+      onChange={(event, value) => {
+        history.push(`${pagePath}/${value.id}`);
       }}
       inputValue={inputValue}
       onInputChange={(event, newInputValue) => {
@@ -29,9 +30,10 @@ function Suggest(props) {
         option: classes.suggestOption,
       }}
       disableClearable
-      options={suggests.map((option) => option.name)}
+      getOptionLabel={(option) => (option.name ? option.name : '')}
+      options={suggests}
       renderOption={(option) => (
-        <Box style={{ overflow: 'hidden' }}>{option}</Box>
+        <Box style={{ overflow: 'hidden' }}>{option.name}</Box>
       )}
       renderInput={(params) => (
         <TextField
@@ -51,19 +53,17 @@ function Suggest(props) {
 }
 
 Suggest.defaultProps = {
-  value: null,
-  setValue: null,
   inputValue: null,
   setInputValue: null,
   suggests: null,
+  pagePath: null,
 };
 
 Suggest.propTypes = {
-  value: PropTypes.string,
-  setValue: PropTypes.func,
   inputValue: PropTypes.string,
   setInputValue: PropTypes.func,
   suggests: PropTypes.arrayOf(PropTypes.object),
+  pagePath: PropTypes.string,
 };
 
 export default Suggest;
